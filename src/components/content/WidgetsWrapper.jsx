bd5056c8222232e6c056c3fbbbdd2fb7d9a9dbfd
Mustafa Salah <mustafasalah99@gmail.com>
2022-04-16 14:31:58 +0200

    fix deletable widget bug

--- src/components/content/WidgetsWrapper.jsx
+++ src/components/content/WidgetsWrapper.jsx
@@ -13,81 +13,81 @@ import SliderWidget from "./../layoutWidgets/SliderWidget";
 import SocialMediaWidget from "./../layoutWidgets/SocialMediaWidget";
 
 const WidgetsWrapper = ({ widget, onUpdate, onAddWidget, onDelete }) => {
-	if (widget.type === undefined) return null;
+    if (widget.type === undefined) return null;
 
-	switch (widget.type) {
-		case "add":
-			return (
-				<AddWidget
-					position={widget.position}
-					onSubmit={{
-						label: "Add Widget",
-						handler: () =>
-							onAddWidget(widget.position, widget.widgetType),
-					}}
-				/>
-			);
+    switch (widget.type) {
+        case "add":
+            return (
+                <AddWidget
+                    position={widget.position}
+                    onSubmit={{
+                        label: "Add Widget",
+                        handler: () =>
+                            onAddWidget(widget.position, widget.widgetType),
+                    }}
+                />
+            );
 
-		case "slider":
-			return <SliderWidget onSubmit={() => onUpdate(widget)} />;
+        case "slider":
+            return <SliderWidget onSubmit={() => onUpdate(widget)} />;
 
-		case "recent":
-			return <RecentWidget onSubmit={() => onUpdate(widget)} />;
+        case "recent":
+            return <RecentWidget onSubmit={() => onUpdate(widget)} />;
 
-		case "ads":
-			return (
-				<AdsWidget
-					onSubmit={() => onUpdate(widget)}
-					onDelete={
-						widget.deletable === "1"
-							? () => onDelete(widget)
-							: undefined
-					}
-				/>
-			);
+        case "ads":
+            return (
+                <AdsWidget
+                    onSubmit={() => onUpdate(widget)}
+                    onDelete={
+                        widget.deletable.toString() === "1"
+                            ? () => onDelete(widget)
+                            : undefined
+                    }
+                />
+            );
 
-		case "category":
-			return (
-				<CategoryWidget
-					onSubmit={() => onUpdate(widget)}
-					onDelete={() => onDelete(widget)}
-				/>
-			);
+        case "category":
+            return (
+                <CategoryWidget
+                    onSubmit={() => onUpdate(widget)}
+                    onDelete={() => onDelete(widget)}
+                />
+            );
 
-		case "social_media":
-			return <SocialMediaWidget onSubmit={() => onUpdate(widget)} />;
+        case "social_media":
+            return <SocialMediaWidget onSubmit={() => onUpdate(widget)} />;
 
-		case "search":
-			return <SearchWidget onSubmit={() => onUpdate(widget)} />;
+        case "search":
+            return <SearchWidget onSubmit={() => onUpdate(widget)} />;
 
-		case "schedule":
-			return <SchedulerWidget onSubmit={() => onUpdate(widget)} />;
+        case "schedule":
+            return <SchedulerWidget onSubmit={() => onUpdate(widget)} />;
 
-		case "selected_shows":
-			return (
-				<PickedShowsWidget
-					onSubmit={() => onUpdate(widget)}
-					onDelete={() => onDelete(widget)}
-				/>
-			);
+        case "selected_shows":
+            return (
+                <PickedShowsWidget
+                    onSubmit={() => onUpdate(widget)}
+                    onDelete={() => onDelete(widget)}
+                />
+            );
 
-		case "about":
-			return <AboutWidget onSubmit={() => onUpdate(widget)} />;
+        case "about":
+            return <AboutWidget onSubmit={() => onUpdate(widget)} />;
 
-		case "links_list":
-			return (
-				<LinksListWidget
-					links={widget.settings.links}
-					onSubmit={() => onUpdate(widget)}
-				/>
-			);
+        case "links_list":
+            return (
+                <LinksListWidget
+                    links={widget.settings.links}
+                    onSubmit={() => onUpdate(widget)}
+                />
+            );
 
-		case "account":
-			return <AccountWidget onSubmit={() => onUpdate(widget)} />;
+        case "account":
+            return <AccountWidget onSubmit={() => onUpdate(widget)} />;
 
-		default:
-			return null;
-	}
+        default:
+            return null;
+    }
 };
 
 export default WidgetsWrapper;
